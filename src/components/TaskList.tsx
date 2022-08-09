dd113e7dcf2718e8f419c359c95fbe271d3a31f1
Raphael Coelho <raphael_coelho@hotmail.com.br>
2022-08-09 15:36:14 -0300

    change: challenge completed

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,29 +1,46 @@
-import { useState } from 'react'
+import { useState } from "react"
 
-import '../styles/tasklist.scss'
+import "../styles/tasklist.scss"
 
-import { FiTrash, FiCheckSquare } from 'react-icons/fi'
+import { FiTrash, FiCheckSquare } from "react-icons/fi"
 
 interface Task {
-  id: number;
-  title: string;
-  isComplete: boolean;
+  id: number
+  title: string
+  isComplete: boolean
 }
 
 export function TaskList() {
-  const [tasks, setTasks] = useState<Task[]>([]);
-  const [newTaskTitle, setNewTaskTitle] = useState('');
+  const [tasks, setTasks] = useState<Task[]>([])
+  const [newTaskTitle, setNewTaskTitle] = useState("")
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (newTaskTitle !== "") {
+      setTasks((prevTasks) => [...prevTasks, { id: Math.random(), title: newTaskTitle, isComplete: false }])
+      setNewTaskTitle("")
+    }
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const selectedTaskIndex = tasks.findIndex((task) => task.id === id)
+    if (selectedTaskIndex >= 0) {
+      const selectedTask = tasks[selectedTaskIndex]
+      setTasks((prevTasks) => [
+        ...prevTasks.filter((task) => task.id !== id),
+        { ...selectedTask, isComplete: !selectedTask.isComplete }
+      ])
+    } else {
+      window.alert("Nenhuma tarefa encontrada com esse id. Reinicie a página para continuar")
+    }
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    const selectedTaskIndex = tasks.findIndex((task) => task.id === id)
+    if (selectedTaskIndex >= 0) {
+      setTasks((prevTasks) => prevTasks.filter((task) => task.id !== id))
+    } else {
+      window.alert("Nenhuma tarefa encontrada com esse id. Reinicie a página para continuar")
+    }
   }
 
   return (
@@ -32,25 +49,25 @@ export function TaskList() {
         <h2>Minhas tasks</h2>
 
         <div className="input-group">
-          <input 
-            type="text" 
-            placeholder="Adicionar novo todo" 
+          <input
+            type="text"
+            placeholder="Adicionar novo todo"
             onChange={(e) => setNewTaskTitle(e.target.value)}
             value={newTaskTitle}
           />
           <button type="submit" data-testid="add-task-button" onClick={handleCreateNewTask}>
-            <FiCheckSquare size={16} color="#fff"/>
+            <FiCheckSquare size={16} color="#fff" />
           </button>
         </div>
       </header>
 
       <main>
         <ul>
-          {tasks.map(task => (
+          {tasks.map((task) => (
             <li key={task.id}>
-              <div className={task.isComplete ? 'completed' : ''} data-testid="task" >
+              <div className={task.isComplete ? "completed" : ""} data-testid="task">
                 <label className="checkbox-container">
-                  <input 
+                  <input
                     type="checkbox"
                     readOnly
                     checked={task.isComplete}
@@ -62,11 +79,10 @@ export function TaskList() {
               </div>
 
               <button type="button" data-testid="remove-task-button" onClick={() => handleRemoveTask(task.id)}>
-                <FiTrash size={16}/>
+                <FiTrash size={16} />
               </button>
             </li>
           ))}
-          
         </ul>
       </main>
     </section>
